2b3157dba431692a77a380325f13b75d40792a4b
diegmero <diegmero12@gmail.com>
2024-12-11 10:14:04 -0500

    terminado listo a despliegue

--- gestion_emprendedores/urls.py
+++ gestion_emprendedores/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from emprendedores.views import EmprendedorViewSet
+from django.views.generic import RedirectView
 
 router = DefaultRouter()
 router.register(r'emprendedores', EmprendedorViewSet)
@@ -25,4 +26,5 @@ router.register(r'emprendedores', EmprendedorViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('', RedirectView.as_view(url='/admin/', permanent=True), name='index'),
 ]
